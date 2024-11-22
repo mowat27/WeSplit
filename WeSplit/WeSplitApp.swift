619d69d2d44afc29e084b0b5cea4bcf179969cf4
Adrian Mowat <adrian@altmetric.com>
2024-11-22 12:11:40 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adrian Mowat on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
